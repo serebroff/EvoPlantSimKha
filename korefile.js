5726f361f03e41ae8ffae36633eefc066f88d32d
Alexey Serebrov <serebroff@gmail.com>
2019-04-14 23:07:45 +0300

    some changes in launch and korefile

--- korefile.js
+++ korefile.js
@@ -1,8 +1,9 @@
 let fs = require('fs');
 let path = require('path');
 let project = new Project('EvoPlantSim');
+project.addDefine('HXCPP_API_LEVEL=332');
 project.targetOptions = {"html5":{},"flash":{},"android":{},"ios":{}};
 project.setDebugDir('build/windows');
 await project.addProject('build/windows-build');
-await project.addProject('d:/Projects/kodestudio_sasha/Kode/resources/app/extensions/kha/Kha');
+await project.addProject('d:/Tools/Kode Studio/resources/app/kodeExtensions/kha/Kha');
 resolve(project);
